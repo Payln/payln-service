12a33eb93988c00b7002f8e0c5fc5ad96bbc3fd0
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-10-17 00:18:49 +0100

    feat: add nodemailer for emails

--- lib/db/migrations/000001_db_init.down.sql
+++ lib/db/migrations/000001_db_init.down.sql
@@ -1,1 +1,4 @@
+ALTER TABLE "businesses" DROP CONSTRAINT IF EXISTS businesses_owner_id_fkey;
+
 DROP TABLE IF EXISTS "businesses";
+DROP TABLE IF EXISTS "users";
--- lib/db/migrations/000001_db_init.up.sql
+++ lib/db/migrations/000001_db_init.up.sql
@@ -1,10 +1,27 @@
 -- Up Migration Script
 
+-- Users Table
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "hashed_password" varchar NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now()),
+  "is_active" boolean NOT NULL DEFAULT true,
+  "is_email_verified" boolean NOT NULL DEFAULT false
+);
+
+-- Businesses Table
 CREATE TABLE "businesses" (
   "id" uuid PRIMARY KEY,
+  "owner_id" bigint NOT NULL,
   "name" varchar NOT NULL,
-  "about" text NOT NULL DEFAULT '',
-  "email" varchar UNIQUE NOT NULL,
+  "description" text NOT NULL DEFAULT '',
+  "general_email" varchar UNIQUE NOT NULL,
+  "dispute_email" varchar UNIQUE NOT NULL,
   "phone_number" varchar NOT NULL DEFAULT '',
   "address" varchar NOT NULL DEFAULT '',
   "city" varchar NOT NULL DEFAULT '',
@@ -13,10 +30,9 @@ CREATE TABLE "businesses" (
   "country" varchar NOT NULL DEFAULT '',
   "website_url" varchar NOT NULL DEFAULT '',
   "profile_image_url" varchar NOT NULL DEFAULT '',
-  "hashed_password" varchar NOT NULL,
-  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL DEFAULT (now()),
-  "is_active" boolean NOT NULL DEFAULT true,
-  "is_email_verified" boolean NOT NULL DEFAULT false
+  "is_active" boolean NOT NULL DEFAULT true
 );
+
+ALTER TABLE "businesses" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE CASCADE;
