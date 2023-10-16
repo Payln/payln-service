90d626a9ec17bba414df805374af2e6efd9d821a
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-10-17 00:18:49 +0100

    feat: add nodemailer for emails

---
+++ lib/mailer/sender.ts
@@ -0,0 +1,85 @@
+import nodemailer from "nodemailer";
+import { SentMessageInfo } from "nodemailer";
+import logger from "../logger/logger";
+import { configs } from "../utils/config";
+
+// Define an interface for the EmailSender
+interface EmailSender {
+  sendEmail(
+    subject: string,
+    content: string,
+    to: string[],
+    cc: string[],
+    bcc: string[],
+    attachFiles: string[]
+  ): Promise<void>;
+}
+
+// Define the GmailSender class that implements the EmailSender interface
+class GmailSender implements EmailSender {
+  private transporter: nodemailer.Transporter;
+
+  constructor(
+    private name: string,
+    private fromEmailAddress: string,
+    private fromEmailPassword: string
+  ) {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: fromEmailAddress,
+        pass: fromEmailPassword,
+      },
+    });
+  }
+
+  async sendEmail(
+    subject: string,
+    content: string,
+    to: string[],
+    cc: string[],
+    bcc: string[],
+    attachFiles: string[]
+  ): Promise<void> {
+    const mailOptions = {
+      from: `"${this.name}" <${this.fromEmailAddress}>`,
+      to: to.join(", "),
+      cc: cc.join(", "),
+      bcc: bcc.join(", "),
+      subject: subject,
+      html: content,
+      attachments: attachFiles.map((file) => ({ path: file })),
+    };
+
+    try {
+      const info: SentMessageInfo = await this.transporter.sendMail(mailOptions);
+      logger.info(`Email sent: ${info.response}`);
+    } catch (error) {
+      throw new Error(`Failed to send email: ${error}`);
+    }
+  }
+}
+
+// Example usage:
+const sender = new GmailSender(
+  configs.EmailSenderName,
+  configs.EmailSenderAddress,
+  configs.EmailSenderPassword
+);
+
+(async () => {
+  try {
+    await sender.sendEmail(
+      "Test Subject",
+      "<p>This is a test email.</p>",
+      ["mrikehchukwuka@gmail.com"],
+      [],
+      [],
+      []
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
