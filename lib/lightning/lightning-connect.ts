a69e0fb3754b471aa1bdb05e58cd9302f7367658
rxbryan <rxbryn@gmail.com>
2023-10-19 05:21:09 +0100

    fix types and lint errors

--- lib/lightning/lightning-connect.ts
+++ lib/lightning/lightning-connect.ts
@@ -1,14 +1,28 @@
-import LnMessage from 'lnmessage'
-import net from 'net'
+import LnMessage from "lnmessage";
+import net from "net";
 
-const NODE_PORT = 9735
+const NODE_PORT = 9735;
+
+class Invoice {
+  status: string = "";
+  expires_at: number = 0;
+}
 
 class Lightning {
-  constructor(ip, node_id, rune, port){
-    this.ip = ip
-    this.node_id = node_id
-    this.port =  port || NODE_PORT
-    this._rune = rune
+  ip: string;
+  port: number;
+  node_id:string;
+  _rune: string;
+  client: LnMessage | null;
+  connected: boolean;
+
+  constructor(ip: string, node_id:string, rune: string, port: number = NODE_PORT){
+    this.ip = ip;
+    this.node_id = node_id;
+    this.port =  port || NODE_PORT;
+    this._rune = rune;
+    this.client = null;
+    this.connected = false;
   }
   
   async connect() {
@@ -17,81 +31,90 @@ class Lightning {
       tcpSocket: new net.Socket(),
       ip: this.ip,
       port: this.port
-    })
+    });
 
-    const connected = await this.client.connect()
-    if (!connected) throw Error(`"Failed to connect to node: ${node_id}, @${ip}:${port}"`)
+    const connected = await this.client.connect();
+    if (!connected) throw Error(`"Failed to connect to node: ${this.node_id}@${this.ip}:${this.port}"`);
 
-    this.connection_open = true
+    this.connected = true;
   }
 
   close() {
-    this.client.disconnect()
-    this.connection_open = false
-  }
-
-  async call(method, payload) {
-    try {
-      const result = await this.client.commando({
-        method: method,
-        params: payload,
-        rune: this._rune
-      })
-      return result
-    } catch (err) {
-      throw Error(err)
+    if (this.client)
+    {
+        this.client.disconnect();
+        this.connected = false;
     }
   }
 
-  async invoice(amountMsat, label, description, expiry=3600) {
-    return await this.call('invoice', {
+  async call(method: string, payload: object) {
+    if (!this.connected) this.connect();
+
+    try {
+      const result = this.client && await this.client.commando({
+        method: method,
+        params: payload,
+        rune: this._rune
+      });
+      return result;
+    } catch (err) {
+      //log
+      throw Error(`An error occurred during ${method} rpc to node: ${this.node_id}@${this.ip}:${this.port}"`);
+    }
+  }
+
+  async invoice(amountMsat: number, label: string, description: string, expiry: number=3600) {
+    return await this.call("invoice", {
       amount_msat: amountMsat,
-      label: label,
-      description: description
-    })
+      label,
+      description,
+      expiry
+    });
   }
 
-  async decode(bolt11) {
-    return await this.call('decode', {string: bolt11})
+  async decode(bolt11: string) {
+    return await this.call("decode", {string: bolt11});
   }
 
-  async listinvoices(label) {
-    return await this.call('listinvoices', {label}) 
+  async listinvoices(label: string) {
+    return await this.call("listinvoices", {label}); 
   }
 
-  async payLn(bolt11, amountMsat) {
-    return await this.call('pay', {bolt11})
+  async payLn(bolt11: string) {
+    return await this.call("pay", {bolt11});
   }
 
-  async payBTC(amountSat, destination) {
-    return await this.call('withdraw', {destination, satoshi: amountSat})
+  async payBTC(amountSat: number, destination: string) {
+    return await this.call("withdraw", {destination, satoshi: amountSat});
   }
 
-  async waitinvoice(label) {
-    return await this.call('waitinvoice', {label})
+  async waitinvoice(label: string) {
+    return await this.call("waitinvoice", {label});
   }
 
-  async verifyInvoice(label) {
-    invoice = await this.listinvoices(label)
-    if (invoice.status != 'paid')
+  async verifyInvoice(label: string) {
+    let inv: Invoice = <Invoice>await this.listinvoices(label);
+    if (inv.status != "paid")
     {
-      paid = false
+      let paid = false;
       while(!paid) {
-        while (invoice.expires_at < Date.now) {
-          invoice = await this.listinvoices(label)
-          paid = (invoice.status == 'paid')
+        while (inv.expires_at < Date.now()) {
+          inv = <Invoice> await this.listinvoices(label);
+          paid = (inv.status == "paid");
         }
       }
     }
 
-    return true
+    return true;
   }
 
   verifyPaymentBTC() {
     //
   }
 
-  async listfunds(spent: bool = false) {
-    return await this.call('listfunds', {spent})
+  async listfunds(spent: boolean = false) {
+    return await this.call("listfunds", {spent});
   }
 }
+
+export {Lightning};
